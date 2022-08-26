9662c98f80992f05b75c40acca3b0e24bc3696b7
NhatBuiQuang <buiquangnhatd13cn5@gmail.com>
2022-08-26 20:28:45 +0700

    Test github

---
+++ Test_Nhat/src/test_nhat/Test_Nhat.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test_nhat;
+import java.util.*;
+/**
+ *
+ * @author WBPC.VN
+ */
+public class Test_Nhat {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a,b;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap so 1: ");
+        a = sc.nextInt();
+        System.out.println("Nhap so 2: ");
+        b = sc.nextInt();
+        int sum = a+b;
+        System.out.println("Tong cua a va b la: " +sum);
+        
+        // TODO code application logic here
+    }
+    
+}
