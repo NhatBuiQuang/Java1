be93bc23b7483981215205a3638aa928edf42975
NhatBuiQuang <buiquangnhatd13cn5@gmail.com>
2022-08-30 19:37:23 +0700

    Nộp bài tập tìm số trong chuỗi

---
+++ Test_Nhat/src/findNumber.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.*;
+
+/**
+ *
+ * @author WBPC.VN
+ */
+public class findNumber {
+
+    public static void main(String[] args) {
+        String str = new String();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap chuoi: ");
+        str = sc.nextLine();
+        char[] check = str.toCharArray();
+        StringBuilder sb = new StringBuilder(); //Lớp StringBuilder để tạo string có thể thay đổi
+        for (char c : check) {
+            if (Character.isDigit(c)) {
+                sb.append(c); //Được sử dụng để phụ thêm (append) chuỗi đã cho với chuỗi này. Phương thức append() được nạp chồng giống dạng append(char),...
+            }
+        }
+        System.out.println(sb + " ");
+    }
+}
